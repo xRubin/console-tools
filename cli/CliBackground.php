57ca734a3cb9c7ee0f5d28da3107d2c45ff822f9
Rubin <dalamar84@mail.ru>
2014-11-09 15:58:26 +0400

    Create CliBackground.php

---
+++ cli/CliBackground.php
@@ -0,0 +1,14 @@
+<?php
+
+class CliBackground {
+
+    const BLACK  = '40';
+    const RED = '41';
+    const GREEN = '42';
+    const YELLOW = '43';
+    const BLUE = '44';
+    const MAGENTA = '45';
+    const CYAN = '46';
+    const LIGHT_GRAY  = '47';
+
+}
