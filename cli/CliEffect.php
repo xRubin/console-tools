864dd0841deab65884ac424e244b5a18746d20a9
Rubin <dalamar84@mail.ru>
2014-11-09 15:58:51 +0400

    Create CliEffect.php

---
+++ cli/CliEffect.php
@@ -0,0 +1,11 @@
+class CliEffect {
+
+    const RESET = '0';
+    const BOLD = '1';
+    const DIM = '2';
+    const UNDERLINE = '4';
+    const BLINK = '5';
+    const INVERT = '7';
+    const HIDDEN = '8';
+
+}
